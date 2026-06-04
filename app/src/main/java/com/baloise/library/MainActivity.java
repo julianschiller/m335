39658d6fb0a823a75d4f8d85a7ad2ba224bc805c
Julian Schiller <julian.schller@stud.aprentas.com>
2026-06-04 14:42:08 +0200

    create menu in Activities

--- app/src/main/java/com/baloise/library/MainActivity.java
+++ app/src/main/java/com/baloise/library/MainActivity.java
@@ -1,6 +1,10 @@
 package com.baloise.library;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.mnu_media) {
+            startActivity(new Intent(this, MainActivity.class));
+        } else if (item.getItemId() == R.id.mnu_borrowing) {
+            startActivity(new Intent(this, ShowBorrowingActivity.class));
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
     }
 }
